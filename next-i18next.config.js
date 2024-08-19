fe9ffb29bc963362c27234773c0ee338acbcc8cc
Stephanides <stefanmacuga1@gmail.com>
2024-08-19 14:13:47 +0200

    Add locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['sk', 'cs', 'en', 'de', 'pl', 'ja', 'hu'],
   },
+  localePath: path.resolve('./public/locales'),
   ns: [
     'common',
     'contact',
