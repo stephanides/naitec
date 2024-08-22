6642ccb212edfe757a24df8944275527044c5ce0
Stephanides <stefanmacuga1@gmail.com>
2024-08-22 15:44:52 +0200

    Add basics

--- next.config.js
+++ next.config.js
@@ -6,59 +6,59 @@ module.exports = {
     NEXT_PUBLIC_MAP_API_KEY: process.env.MAP_API_KEY,
     NEXT_PUBLIC_API_TOKEN: process.env.API_TOKEN,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/kontakt',
-        destination: '/contact',
-      },
-      {
-        source: '/kapcsolat',
-        destination: '/contact',
-      },
-      {
-        source: '/podpora',
-        destination: '/support',
-      },
-      {
-        source: '/wsparcie',
-        destination: '/support',
-      },
-      {
-        source: '/rolunk',
-        destination: '/support',
-      },
-      {
-        source: '/krbovy-difuzor',
-        destination: '/fireplace-diffuser',
-      },
-      {
-        source: '/kamin-difuzor',
-        destination: '/fireplace-diffuser',
-      },
-      { source: '/dyfuzor-kominkowy', destination: '/fireplace-diffuser' },
-      { source: '/kandallo-difuzor', destination: '/fireplace-diffuser' },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/locales/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-          {
-            key: 'Pragma',
-            value: 'no-cache',
-          },
-          {
-            key: 'Expires',
-            value: '0',
-          },
-        ],
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/kontakt',
+  //       destination: '/contact',
+  //     },
+  //     {
+  //       source: '/kapcsolat',
+  //       destination: '/contact',
+  //     },
+  //     {
+  //       source: '/podpora',
+  //       destination: '/support',
+  //     },
+  //     {
+  //       source: '/wsparcie',
+  //       destination: '/support',
+  //     },
+  //     {
+  //       source: '/rolunk',
+  //       destination: '/support',
+  //     },
+  //     {
+  //       source: '/krbovy-difuzor',
+  //       destination: '/fireplace-diffuser',
+  //     },
+  //     {
+  //       source: '/kamin-difuzor',
+  //       destination: '/fireplace-diffuser',
+  //     },
+  //     { source: '/dyfuzor-kominkowy', destination: '/fireplace-diffuser' },
+  //     { source: '/kandallo-difuzor', destination: '/fireplace-diffuser' },
+  //   ];
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/locales/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Cache-Control',
+  //           value: 'no-cache, no-store, must-revalidate',
+  //         },
+  //         {
+  //           key: 'Pragma',
+  //           value: 'no-cache',
+  //         },
+  //         {
+  //           key: 'Expires',
+  //           value: '0',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
