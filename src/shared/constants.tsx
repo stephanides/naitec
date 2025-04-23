90994ba82e46e0e1618eb1d22cebf0d89bc23718
Stephanides <stefanmacuga1@gmail.com>
2025-04-23 15:35:13 +0200

    Change HU number

--- src/shared/constants.tsx
+++ src/shared/constants.tsx
@@ -53,7 +53,7 @@ export const EMAILS = {
 
 export const PHONE_NUMBERS = {
   CONTACT_AT: '+43 650 2749692',
-  CONTACT_HU: '+36 20 777 4477',
+  CONTACT_HU: '+36 20 777 5577',
   CONTACT_SK: '+421 917 142 089',
 };
 
